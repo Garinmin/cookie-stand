0214ff3c6246bb9a7dcf57ebbd4484579897d94d
Garinmin <garinmin@gmail.com>
2021-03-18 20:10:48 -0700

    added form

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const timeSlots = [
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 var hourlySales = new Array();
@@ -31,80 +31,155 @@ function hourlySalesIns(min, max, avgCookie){
   return hourlySales;
 }
 
-const seattle = {
-  location: 'Seattle',
-  min: 20,
-  max: 30,
-  avgCookie: 5,
-  salesIns: function(){
-    return hourlySalesIns(this.max, this.min, this.avgCookie);
-  },
-};
-seattle.hourlySales = Array.from(seattle.salesIns());
-const tokyo = {
-  location: 'Tokyo',
-  min: 20,
-  max: 40,
-  avgCookie: 5,
-  salesIns: function(){
-    return hourlySalesIns(this.max, this.min, this.avgCookie);
-  },
-};
-tokyo.hourlySales = Array.from(tokyo.salesIns());
+const allLocations = [];
 
-const amman = {
-  location: 'Amman',
-  min: 10,
-  max: 30,
-  avgCookie: 5,
-  salesIns: function(){
-    return hourlySalesIns(this.max, this.min, this.avgCookie);
-  },
-};
-amman.hourlySales = Array.from(amman.salesIns());
-const storeContainerElem = document.getElementById('store-container');
+const table = document.getElementById('cookie-chart');
 
-renderCookieStand(seattle);
-renderCookieStand(tokyo);
-renderCookieStand(amman);
-
-function renderCookieStand(cookieStand) {
-
-  const sectionElem = document.createElement('section');
-  storeContainerElem.appendChild(sectionElem);
-
-  const locationElem = document.createElement('h2');
-  sectionElem.appendChild(locationElem);
-  locationElem.textContent = cookieStand.location;
-
-  const hourListElem = document.createElement('ul');
-  sectionElem.appendChild(hourListElem);
-
-  for (let i = 0; i < timeSlots.length; i += 1) {
-
-    const hourItemElem = document.createElement('li');
-
-    hourListElem.appendChild(hourItemElem);
-
-    const currentTimeSlot = timeSlots[i];
-
-    const currentSales = cookieStand.hourlySales[i];
-
-    hourItemElem.textContent = currentTimeSlot + ': ' + currentSales + ' cookies';
-  }
-
-
+function LocationStand(name, min, max, avgCookie) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avgCookie = avgCookie;
+  this.hourlySales = Array.from(hourlySalesIns(min, max, avgCookie));
   let total = 0;
-  // the sum of all the sales for the current cookieStand
-
-  for (let i = 0; i < cookieStand.hourlySales.length; i += 1) {
-    const currentSales = cookieStand.hourlySales[i];
-    total += currentSales;
+  for (let i = 0; i < this.hourlySales.length; i += 1) {
+    total += this.hourlySales[i];
   }
-  /*   total = total.toFixed(); */
-  // need last total item
-  const totalItemElem = document.createElement('li');
-  hourListElem.appendChild(totalItemElem);
-  totalItemElem.textContent = 'Total: ' + total + ' cookies';
+  this.total=total;
+  allLocations.push(this);
+}
+let total6am = 0;
+let total7am = 0;
+let total8am = 0;
+let total9am = 0;
+let total10am = 0;
+let total11am = 0;
+let total12am = 0;
+let total1pm = 0;
+let total2pm = 0;
+let total3pm = 0;
+let total4pm = 0;
+let total5pm = 0;
+let total6pm = 0;
+let total7pm = 0;
+LocationStand.prototype.render = function () {
+  const row = createChild('tr', table);
+  const name = this.name;
+  createChild('td', row, name);
+  // const propertyNames = ['name', 'min', 'max', 'avgCookie'];
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    const currentHourlySales = this.hourlySales[i];
+    createChild('td', row, currentHourlySales);
+  }
+  createChild('td', row, this.total);
+  total6am = total6am + this.hourlySales[0];
+  total7am = total7am + this.hourlySales[1];
+  total8am = total8am + this.hourlySales[2];
+  total9am = total9am + this.hourlySales[3];
+  total10am = total10am + this.hourlySales[4];
+  total11am = total11am + this.hourlySales[5];
+  total12am = total12am + this.hourlySales[6];
+  total1pm = total1pm + this.hourlySales[7];
+  total2pm = total2pm + this.hourlySales[8];
+  total3pm = total3pm + this.hourlySales[9];
+  total4pm = total4pm + this.hourlySales[10];
+  total5pm = total5pm + this.hourlySales[11];
+  total6pm = total6pm + this.hourlySales[12];
+  total7pm = total7pm + this.hourlySales[13];
+};
+
+function createChild(tag, parent, text) {
+  const child = document.createElement(tag);
+  parent.appendChild(child);
+  if (text !== undefined) {
+    child.textContent = text;
+  }
+  return child;
+}
+
+function createHeaderRow() {
+
+  const row = createChild('tr', table);
+
+  const columnNames = ['name'];
+
+  for (let i = 0; i < columnNames.length; i++) {
+    createChild('th', row, columnNames[i]);
+  }
+  for (let i=0; i<timeSlots.length; i++){
+    createChild('th', row, timeSlots[i]);
+  }
+  createChild('th', row, 'Total');
+}
+
+
+function renderLocations() {
+  for (let i = 0; i < allLocations.length; i++) {
+    allLocations[i].render();
+  }
+}
+
+function createFooterRow() {
+  const row = createChild('tr', table);
+  createChild('th', row, 'Total');
+  createChild('th', row, total6am);
+  createChild('th', row, total7am);
+  createChild('th', row, total8am);
+  createChild('th', row, total9am);
+  createChild('th', row, total10am);
+  createChild('th', row, total11am);
+  createChild('th', row, total12am);
+  createChild('th', row, total1pm);
+  createChild('th', row, total2pm);
+  createChild('th', row, total3pm);
+  createChild('th', row, total4pm);
+  createChild('th', row, total5pm);
+  createChild('th', row, total6pm);
+  createChild('th', row, total7pm);
+  let total = total6am + total7am + total8am + total9am + total10am + total11am + total12am + total1pm + total2pm + total3pm + total4pm + total5pm + total6pm + total7pm;
+  createChild('th', row, total);
+
 
 }
+
+// listen for submits
+
+function addCookieLocation(event) {
+  event.preventDefault();
+
+  const name = event.target.name.value;
+  const min = event.target.min.value;
+  const max = event.target.max.value;
+  let avgCookie = event.target.avgCookie.value;
+
+  new LocationStand(name, min, max, avgCookie);
+
+  table.innerHTML = '';
+
+  renderTable();
+
+  event.target.reset();
+
+}
+
+function renderTable() {
+
+  createHeaderRow();
+
+  renderLocations();
+
+  createFooterRow();
+}
+
+// wire up event listener
+const addCookieForm = document.getElementById('add-location-form');
+addCookieForm.addEventListener('submit', addCookieLocation);
+
+new LocationStand('Seattle', 23, 65, 6.3);
+new LocationStand('Tokyo', 3, 24, 1.2);
+new LocationStand('Dubai', 11, 38, 3.7);
+new LocationStand('Paris', 20, 38, 2.3);
+new LocationStand('Lima', 2, 16, 4.6);
+
+
+renderTable();
--- app2.js
+++ app2.js
@@ -37,27 +37,58 @@ const allKittens = [];
 const table = document.getElementById('kitten-chart');
 
 
-function Kitten(name, breed, interests, coatColor) {
+function Kitten(name, min, max, avgCookie) {
   this.name = name;
-  this.breed = breed || 'unknown';
-  this.interests = interests || 'none';
-  this.coatColor = coatColor || 'not specified';
-
-
+  this.min = min;
+  this.max = max;
+  this.avgCookie = avgCookie;
+  this.hourlySales = Array.from(hourlySalesIns(min, max, avgCookie));
+  let total = 0;
+  for (let i = 0; i < this.hourlySales.length; i += 1) {
+    const currentSales = this.hourlySales[i];
+    total += currentSales;
+  }
+  this.total=total;
   allKittens.push(this);
 }
-
+let total6am = 0;
+let total7am = 0;
+let total8am = 0;
+let total9am = 0;
+let total10am = 0;
+let total11am = 0;
+let total12am = 0;
+let total1pm = 0;
+let total2pm = 0;
+let total3pm = 0;
+let total4pm = 0;
+let total5pm = 0;
+let total6pm = 0;
+let total7pm = 0;
 Kitten.prototype.render = function () {
   const row = createChild('tr', table);
-  const propertyNames = ['name', 'breed', 'interests', 'coatColor'];
-  for (let i = 0; i < propertyNames.length; i++) {
-    const currentPropertyName = propertyNames[i];
-
-    // common use for bracket notation instead of dot notation
-    const currentPropertyValue = this[currentPropertyName];
-    createChild('td', row, currentPropertyValue);
+  const name = this.name
+  createChild('td', row, name);
+ // const propertyNames = ['name', 'min', 'max', 'avgCookie'];
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    const currentHourlySales = this.hourlySales[i];
+    createChild('td', row, currentHourlySales);
   }
-
+  createChild('td', row, this.total);
+  total6am = total6am + this.hourlySales[0];
+  total7am = total7am + this.hourlySales[1];
+  total8am = total8am + this.hourlySales[2];
+  total9am = total9am + this.hourlySales[3];
+  total10am = total10am + this.hourlySales[4];
+  total11am = total11am + this.hourlySales[5];
+  total12am = total12am + this.hourlySales[6];
+  total1pm = total1pm + this.hourlySales[7];
+  total2pm = total2pm + this.hourlySales[8];
+  total3pm = total3pm + this.hourlySales[9];
+  total4pm = total4pm + this.hourlySales[10];
+  total5pm = total5pm + this.hourlySales[11];
+  total6pm = total6pm + this.hourlySales[12];
+  total7pm = total7pm + this.hourlySales[13];
 }
 
 function createChild(tag, parent, text) {
@@ -73,21 +104,17 @@ function createHeaderRow() {
 
   const row = createChild('tr', table);
 
-  const columnNames = ['name', 'breed', 'interests', 'coatColor'];
+  const columnNames = ['name'];
 
   for (let i = 0; i < columnNames.length; i++) {
     createChild('th', row, columnNames[i]);
   }
+  for (let i=0; i<timeSlots.length; i++){
+      createChild('th', row, timeSlots[i]);
+  }
+  createChild('th', row, 'Total');
 }
 
-function createFooterRow() {
-
-  const row = createChild('tr', table);
-
-  const th = createChild('th', row, 'Kittens to Adopt ' + allKittens.length);
-  th.setAttribute('colspan', '7');
-
-}
 
 function renderKittens() {
   for (let i = 0; i < allKittens.length; i++) {
@@ -95,7 +122,28 @@ function renderKittens() {
   }
 }
 
+function createFooterRow() {
+  const row = createChild('tr', table);
+  createChild('th', row, 'Total');
+  createChild('th', row, total6am);
+  createChild('th', row, total7am);
+  createChild('th', row, total8am);
+  createChild('th', row, total9am);
+  createChild('th', row, total10am);
+  createChild('th', row, total11am);
+  createChild('th', row, total12am);
+  createChild('th', row, total1pm);
+  createChild('th', row, total2pm);
+  createChild('th', row, total3pm);
+  createChild('th', row, total4pm);
+  createChild('th', row, total5pm);
+  createChild('th', row, total6pm);
+  createChild('th', row, total7pm);
+  let total = total6am + total7am + total8am + total9am + total10am + total11am + total12am + total1pm + total2pm + total3pm + total4pm + total5pm + total6pm + total7pm;
+  createChild('th', row, total);
+  
 
+}
 
 // listen for submits
 
@@ -103,12 +151,12 @@ function addKittenHandler(event) {
   event.preventDefault();
 
   const name = event.target.name.value;
-  const breed = event.target.breed.value;
-  const coatColor = event.target.coatColor.value;
-  let interests = event.target.interests.value;
+  const min = event.target.min.value;
+  const max = event.target.max.value;
+  let avgCookie = event.target.avgCookie.value;
   // interests = interests.split(',');
   
-  const newKitten = new Kitten(name, breed, interests, coatColor);
+  const newKitten = new Kitten(name, min, max, avgCookie);
 
   table.innerHTML = '';
 
